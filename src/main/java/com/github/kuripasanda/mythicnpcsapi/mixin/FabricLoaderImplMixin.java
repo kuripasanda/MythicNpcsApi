0321cf43fba3be2b245abe25e7991ade832c0017
kuripasanda <ytkuripachanda@gmail.com>
2026-03-20 22:08:09 +0900

    [Change] Mixin

--- src/main/java/com/github/kuripasanda/mythicnpcsapi/mixin/FabricLoaderImplMixin.java
+++ src/main/java/com/github/kuripasanda/mythicnpcsapi/mixin/FabricLoaderImplMixin.java
@@ -2,13 +2,13 @@ package com.github.kuripasanda.mythicnpcsapi.mixin;
 
 import com.github.kuripasanda.mythicnpcsapi.MinecraftServerBridge;
 import com.github.kuripasanda.mythicnpcsapi.MythicNpcsApi;
-import net.minecraft.server.MinecraftServer;
+import net.fabricmc.loader.impl.FabricLoaderImpl;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(value = MinecraftServer.class, remap = false )
-public class MinecraftServerMixin implements MinecraftServerBridge {
+@Mixin(value = FabricLoaderImpl.class, remap = false )
+public class FabricLoaderImplMixin implements MinecraftServerBridge {
 
     @Unique
     protected MythicNpcsApi mythicNpcsApi$api;
